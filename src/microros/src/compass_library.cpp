a2ce3c1fff11d2acd5839049808a7b8bf003207f
shame5800 <shanepomajambo16@gmail.com>
2025-09-07 10:24:00 -0400

    translated compass library to C++

---
+++ src/microros/src/compass_library.cpp
@@ -0,0 +1,126 @@
+//Orignal naming, cmps14_compass_library.h
+//Spell Library correctly
+//goal is to created a object orientated system
+//Method names have removed cmps14_ prefix due to now being called with cmps14.methodName();
+
+#ifndef CMPS14_COMPASS_LIBRARY_H  
+#define CMPS14_COMPASS_LIBRARY_H 
+
+#include "hardware/i2c.h"
+#include <cmath>  
+
+//CMPS14 Register Definitions
+#define CONTROL_Register 0x00
+#define BEARING_Register 0x02
+#define PITCH_Register 0x04
+#define ROLL_Register 0x05
+#define MAGNETX_Register 0x06
+#define MAGNETY_Register 0x08
+#define MAGNETZ_Register 0x0A
+#define ACCELEROX_Register 0x20
+#define ACCELEROY_Register 0x22
+#define ACCELEROZ_Register 0x24
+#define GYROX_Register 0x25
+#define GYROY_Register 0x27
+#define GYROZ_Register 0x29
+#define ROLLP_Register 0x1C
+#define PITCHP_Register 0x1A
+#define Calibration_Register 0x1E
+
+#define ACCELEROMETER_SCALE 9.80592991914 / 1000.0 // 1 m/s^2
+#define GYROSCOPE_SCALE 1.0 / 16.0              // 1 Dps
+#define MAGNETOMETER_SCALE 1.0                   // No clue
+
+class cmps14{
+public:
+    int bearing;
+    signed char pitch;
+    signed char roll;
+    uint8_t i2cAddress;
+    i2c_inst_t i2cPort;
+
+    //Initalizer/Constructor
+    cmps14(i2c_inst_t* port, uint8_t address) 
+        : bearing(0), pitch(0), roll(0), i2cAddress(address), i2cPort(*port) {} 
+
+    int writeByte(uint8_t reg, uint8_t value){
+        uint8_t data[] = {reg, value};
+        int result = i2c_write_blocking(&i2cPort,i2cAddress,data,2,false);
+        return result;
+        
+    }
+
+    int readBytes(uint8_t reg, uint8_t *buffer, uint8_t length){
+        i2c_write_blocking(&i2cPort, i2cAddress, &reg, 1, true);
+        int result = i2c_read_blocking(&i2cPort, i2cAddress, buffer, length, false);
+        return result;
+    }
+
+    int16_t getBearing() {
+        uint8_t buffer[2];
+        if (readBytes(BEARING_Register, buffer, 2) != 2) {
+        return -1;
+        }
+        int16_t result = ((buffer[0] << 8) | buffer[1]);
+        return result;
+
+    }
+
+    int8_t getPitch() {
+    uint8_t buffer;
+    if (readBytes(PITCH_Register, &buffer, 1) != 1) {
+        return 0;
+    }
+    return (int8_t)buffer;
+}
+
+    int8_t getRoll() {
+    uint8_t buffer;
+    if (readBytes(ROLL_Register, &buffer, 1) != 1) {
+        return 0;
+    }
+    return (int8_t)buffer;
+}
+
+void readAccelerator(float *accelX, float *accelY, float *accelZ) {
+    uint8_t buffer[6];
+    if (readBytes(ACCELEROX_Register, buffer, 6) != 6) {
+        *accelX = 0;
+        *accelY = 0;
+        *accelZ = 0;
+        return;
+    }
+    *accelX = ((int16_t)(buffer[0] << 8 | buffer[1])) * ACCELEROMETER_SCALE;
+    *accelY = ((int16_t)(buffer[2] << 8 | buffer[3])) * ACCELEROMETER_SCALE;
+    *accelZ = ((int16_t)(buffer[4] << 8 | buffer[5])) * ACCELEROMETER_SCALE;
+}
+
+void readGyro(float *gyroX, float *gyroY, float *gyroZ) {
+    uint8_t buffer[6];
+    if (readBytes(GYROX_Register, buffer, 6) != 6) {
+        *gyroX = 0;
+        *gyroY = 0;
+        *gyroZ = 0;
+        return;
+    }
+    *gyroX = ((int16_t)(buffer[0] << 8 | buffer[1])) * GYROSCOPE_SCALE;
+    *gyroY = ((int16_t)(buffer[2] << 8 | buffer[3])) * GYROSCOPE_SCALE;
+    *gyroZ = ((int16_t)(buffer[4] << 8 | buffer[5])) * GYROSCOPE_SCALE;
+}
+
+void readMagnet(float *magnetX, float *magnetY, float *magnetZ) {
+    uint8_t buffer[6];
+    if (readBytes(MAGNETX_Register, buffer, 6) != 6) {
+        *magnetX = 0;
+        *magnetY = 0;
+        *magnetZ = 0;
+        return;
+    }
+    *magnetX = ((int16_t)(buffer[0] << 8 | buffer[1])) * MAGNETOMETER_SCALE;
+    *magnetY = ((int16_t)(buffer[2] << 8 | buffer[3])) * MAGNETOMETER_SCALE;
+    *magnetZ = ((int16_t)(buffer[4] << 8 | buffer[5])) * MAGNETOMETER_SCALE;
+}
+
+};//end cmps14        // WHY TF IS THERE A SEMICOLON O U T S I D E THE PARENTHESIS BRACKET idk what they called
+
+#endif   
