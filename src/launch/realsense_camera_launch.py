12adf1aaf6a5ac82415118caf9d3adb1c0e36412
Chris Nassif <chrisjnassif@gmail.com>
2025-09-29 00:11:04 +0000

    added some stuff for the gitignore and camera

---
+++ src/launch/realsense_camera_launch.py
@@ -0,0 +1,20 @@
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from ament_index_python.packages import get_package_share_directory
+import os
+
+
+def generate_launch_description():
+    my_package_share_dir = get_package_share_directory("realsense2_camera")
+
+    included_python_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(os.path.join(my_package_share_dir, "launch", "rs_launch.py")),
+        launch_arguments=[("color_qos", "SENSOR_DATA"), ("depth_qos", "SENSOR_DATA"), ("enable_depth", "false")],
+    )
+
+    return LaunchDescription(
+        [
+            included_python_launch,
+        ]
+    )
