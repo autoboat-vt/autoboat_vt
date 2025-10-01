e7bef32bdb996b15eea5640325f1531ecefb9f05
Davis <chalmersdavis9198@gmail.com>
2025-10-01 15:42:18 -0400

    added proper h files

---
+++ src/microros/CMakeFiles/pico_microros.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for pico_microros.
+# This may be replaced when dependencies are built.
---
+++ src/microros/_deps/picotool-build/data_locs.cpp
@@ -0,0 +1,7 @@
+
+#include <vector>
+#include <string>
+
+std::vector<std::string> data_locs = {
+    "./","/home/ws/src/microros/_deps/picotool/"
+};
