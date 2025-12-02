9b85955d543c5cdd0b27e0e0f3de49cb675c1243
Chris Nassif <chrisjnassif@gmail.com>
2025-12-02 11:49:24 -0500

    trying to fix pyqt installation bug for arm

--- build_dockerfile_commands.sh
+++ build_dockerfile_commands.sh
@@ -1,5 +1,15 @@
-docker build -t vtautoboat/autoboat_docker_dev_image -f .devcontainer/Dockerfile .
-docker build -t vtautoboat/autoboat_docker_dev_image:microros -f .devcontainer/devcontainer_variants/Dockerfile.microros .
-docker build -t vtautoboat/autoboat_docker_dev_image:deepstream -f .devcontainer/devcontainer_variants/Dockerfile.deepstream .
-docker build -t vtautoboat/autoboat_docker_dev_image:yolo -f .devcontainer/devcontainer_variants/Dockerfile.yolo .
-docker build -t vtautoboat/autoboat_docker_dev_image:deepstream_and_yolo -f .devcontainer/devcontainer_variants/Dockerfile.deepstream_and_yolo .
+
+
+# Images for AMD/ Intel CPUs
+docker build -t vtautoboat/autoboat_docker_dev_image --platform linux/amd64 -f .devcontainer/Dockerfile .
+docker build -t vtautoboat/autoboat_docker_dev_image:microros --platform linux/amd64 -f .devcontainer/devcontainer_variants/Dockerfile.microros .
+docker build -t vtautoboat/autoboat_docker_dev_image:deepstream --platform linux/amd64 -f .devcontainer/devcontainer_variants/Dockerfile.deepstream .
+docker build -t vtautoboat/autoboat_docker_dev_image:yolo --platform linux/amd64 -f .devcontainer/devcontainer_variants/Dockerfile.yolo .
+docker build -t vtautoboat/autoboat_docker_dev_image:deepstream_and_yolo --platform linux/amd64 -f .devcontainer/devcontainer_variants/Dockerfile.deepstream_and_yolo .
+
+# Images for ARM CPUs or the Jetson
+docker build -t vtautoboat/autoboat_docker_dev_image --platform linux/arm64 -f .devcontainer/Dockerfile .
+docker build -t vtautoboat/autoboat_docker_dev_image:microros --platform linux/arm64 -f .devcontainer/devcontainer_variants/Dockerfile.microros .
+docker build -t vtautoboat/autoboat_docker_dev_image:deepstream --platform linux/arm64 -f .devcontainer/devcontainer_variants/Dockerfile.deepstream .
+docker build -t vtautoboat/autoboat_docker_dev_image:yolo --platform linux/arm64 -f .devcontainer/devcontainer_variants/Dockerfile.yolo .
+docker build -t vtautoboat/autoboat_docker_dev_image:deepstream_and_yolo --platform linux/arm64 -f .devcontainer/devcontainer_variants/Dockerfile.deepstream_and_yolo .
