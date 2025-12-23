aa3784c824b71bb224c16fffc9129a3bc04f91d3
Chris Nassif <chrisjnassif@gmail.com>
2025-12-23 17:44:59 -0500

    removed docker buildx build from the build_and_push_all_docker_images_commands

--- build_and_push_all_docker_images_commands.sh
+++ build_and_push_all_docker_images_commands.sh
@@ -1,11 +1,11 @@
 sudo docker login
 
 # Images for AMD/ Intel CPUs
-docker buildx build -t vtautoboat/autoboat_docker_dev_image:latest --platform linux/amd64 -f .devcontainer/Dockerfile .
-docker buildx build -t vtautoboat/autoboat_docker_dev_image:microros --platform linux/amd64 -f .devcontainer/devcontainer_variants/Dockerfile.microros .
-docker buildx build -t vtautoboat/autoboat_docker_dev_image:deepstream --platform linux/amd64 -f .devcontainer/devcontainer_variants/Dockerfile.deepstream .
-docker buildx build -t vtautoboat/autoboat_docker_dev_image:yolo --platform linux/amd64 -f .devcontainer/devcontainer_variants/Dockerfile.yolo .
-docker buildx build -t vtautoboat/autoboat_docker_dev_image:deepstream_and_yolo --platform linux/amd64 -f .devcontainer/devcontainer_variants/Dockerfile.deepstream_and_yolo .
+docker build -t vtautoboat/autoboat_docker_dev_image:latest --platform linux/amd64 -f .devcontainer/Dockerfile .
+docker build -t vtautoboat/autoboat_docker_dev_image:microros --platform linux/amd64 -f .devcontainer/devcontainer_variants/Dockerfile.microros .
+docker build -t vtautoboat/autoboat_docker_dev_image:deepstream --platform linux/amd64 -f .devcontainer/devcontainer_variants/Dockerfile.deepstream .
+docker build -t vtautoboat/autoboat_docker_dev_image:yolo --platform linux/amd64 -f .devcontainer/devcontainer_variants/Dockerfile.yolo .
+docker build -t vtautoboat/autoboat_docker_dev_image:deepstream_and_yolo --platform linux/amd64 -f .devcontainer/devcontainer_variants/Dockerfile.deepstream_and_yolo .
 
 
 docker push vtautoboat/autoboat_docker_dev_image:latest
