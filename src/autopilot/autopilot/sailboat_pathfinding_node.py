98fc417b34d3ce11e602c794319db7937f3542e9
ramen <raymondhong42@gmail.com>
2026-01-21 23:16:11 -0500

    creating position subscriber

--- src/autopilot/autopilot/sailboat_pathfinding_node.py
+++ src/autopilot/autopilot/sailboat_pathfinding_node.py
@@ -1,18 +1,24 @@
 import rclpy
 from rclpy.node import Node
+from sensor_msgs.msg import NavSatFix
+from .autopilot_library.utils import *
 
-class PathNode(Node):
+class PathfindingNode(Node):
 
     def __init__(self):
         super().__init__("sailboat_pathfinding")
         self.get_logger().info("working")
-        # create publisher stuff here
-        
+        # create publisher stuff here (later)
 
+        # subscribing to position
+        self.gps_subscriber=self.create_subscription(NavSatFix, "/position", self.gps_call, 10)
+
+    def gps_call(self,msg: NavSatFix): # argument in function is a shorthand for calling msg as object of NavSatFix
+        self.get_logger().info(str(msg))
+        
 def main():
     rclpy.init()
-    node = PathNode()
+    node = PathfindingNode()
     rclpy.spin(node)
-
     node.destroy_node()
     rclpy.shutdown()
