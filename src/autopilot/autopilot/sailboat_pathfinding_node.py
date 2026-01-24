d72b59338271a307b8a5bd78851b5ea033437cbc
ramen <raymondhong42@gmail.com>
2026-01-24 15:47:07 -0500

    adding waypoint publisher

--- src/autopilot/autopilot/sailboat_pathfinding_node.py
+++ src/autopilot/autopilot/sailboat_pathfinding_node.py
@@ -1,8 +1,8 @@
 import rclpy
 from rclpy.node import Node
 from sensor_msgs.msg import NavSatFix
+from autoboat_msgs.msg import WaypointList
 from rclpy.qos import qos_profile_sensor_data
-from .autopilot_library.utils import *
 
 class PathfindingNode(Node):
 
@@ -13,10 +13,15 @@ class PathfindingNode(Node):
 
         # subscribing to position
         self.gps_subscriber=self.create_subscription(NavSatFix, "/position", self.gps_call, qos_profile_sensor_data)
+        self.waypoint_subscriber=self.create_subscription(WaypointList, "/waypoints_list", self.waypointcall, qos_profile_sensor_data)
 
     def gps_call(self,msg: NavSatFix): # argument in function is a shorthand for calling msg as object of NavSatFix
-        self.get_logger().info("latitude: " + str(msg.latitude) + " longitude: " + str(msg.longitude))
+        # self.get_logger().info("latitude: " + str(msg.latitude) + " longitude: " + str(msg.longitude))
+        pass
         
+    def waypointcall(self,msg: WaypointList): # function calling for waypoints once sent
+        self.get_logger().info(str(msg))
+
 def main():
     rclpy.init()
     node = PathfindingNode()
