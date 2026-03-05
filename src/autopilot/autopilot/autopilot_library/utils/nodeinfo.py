5a0b411bc5a0265827197da6a81b6f7856ebeeb2
ramen <raymondhong42@gmail.com>
2026-03-05 15:35:40 -0500

    implemented astar algorithm to pathfinding algorithm

--- src/autopilot/autopilot/autopilot_library/utils/nodeinfo.py
+++
@@ -1,8 +0,0 @@
-class NodeInfo:
-    def __init__(self):
-        # coordinates
-        self.oldi = 0 
-        self.oldj = 0
-        self.f = float('inf') # total cost, default infinity
-        self.g = float('inf') # heuristic cost, default infinity
-        self.h = 0 # cumulative movement cost
