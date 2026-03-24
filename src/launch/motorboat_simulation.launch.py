7ece14634164b6c88a771ffedb9e89e6669150c9
Chris Nassif <chrisjnassif@gmail.com>
2026-03-24 17:56:13 -0400

    cleaned some stuff up and removed tests_require in setup.py in ros packages

--- src/launch/motorboat_simulation.launch.py
+++ src/launch/motorboat_simulation.launch.py
@@ -14,14 +14,6 @@ def generate_launch_description():
     gz_launch_path = PathJoinSubstitution([ros_gz_sim_pkg_path, 'launch','gz_sim.launch.py'])
 
     return LaunchDescription([
-        # SetEnvironmentVariable(
-        #     'GZ_SIM_RESOURCE_PATH',
-        #     PathJoinSubstitution([example_pkg_path, 'models'])
-        # ),
-        # SetEnvironmentVariable(
-        #     'GZ_SIM_PLUGIN_PATH',
-        #     PathJoinSubstitution([example_pkg_path, 'plugins'])
-        # ),
         IncludeLaunchDescription(
             PythonLaunchDescriptionSource(gz_launch_path),
             launch_arguments={
@@ -30,8 +22,6 @@ def generate_launch_description():
             }.items(),
         ),
 
-        # Bridging and remapping Gazebo topics to ROS 2 (replace with your own topics)
-         
         Node(
             package='autopilot_transform',
             executable='autopilot_transform',
@@ -58,7 +48,6 @@ def generate_launch_description():
 
             respawn=True,
             respawn_delay=2.0,
-            # remappings=[('/motorboat_simulation/position', '/position')],
             output="log"
         ),
         Node(
