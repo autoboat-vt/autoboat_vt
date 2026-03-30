62f54721bf859a5c3d9c9f7abbc6822e6c06e88c
Barrett Wise <barrettw@fastmail.com>
2026-03-29 21:46:19 -0400

    move sail related stuff to the sailboat payload

--- src/autopilot/autopilot/autopilot_library/utils/telemetry_payloads.py
+++ src/autopilot/autopilot/autopilot_library/utils/telemetry_payloads.py
@@ -40,8 +40,6 @@ class BoatStatusPayload(ctypes.LittleEndianStructure):
         ("desired_rudder_angle", ctypes.c_float),
         ("current_rudder_angle", ctypes.c_float),
         ("rudder_angle_error", ctypes.c_float),
-        ("current_sail_angle", ctypes.c_float),
-        ("sail_angle_error", ctypes.c_float),
     )
 
     one_byte_fields: Final[tuple[tuple[str, ctypes._SimpleCData], ...]] = (
@@ -98,7 +96,9 @@ class SailboatStatusPayload(BoatStatusPayload):
         ("true_wind_angle", ctypes.c_float),
         ("apparent_wind_speed", ctypes.c_float),
         ("apparent_wind_angle", ctypes.c_float),
+        ("current_sail_angle", ctypes.c_float),
         ("desired_sail_angle", ctypes.c_float),
+        ("sail_angle_error", ctypes.c_float),
     )
 
     sail_one_byte_fields: Final[tuple[tuple[str, ctypes._SimpleCData], ...]] = (("full_autonomy_maneuver", ctypes.c_ubyte),)
