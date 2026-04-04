62379a061d0eb59b3abfba74a98371ba69a77d28
Chris Nassif <chrisjnassif@gmail.com>
2026-04-04 18:12:41 -0400

    added new constructor and docstrings for the c++ position object

--- src/autopilot_cpp/autopilot_library/position.cpp
+++ src/autopilot_cpp/autopilot_library/position.cpp
@@ -2,31 +2,41 @@
 #include "geographic_function_library.hpp"
 
 
-Position::Position(double longitude_, double latitude_) {
-    longitude = longitude_;
-    latitude = latitude_;
+Position::Position(double longitude, double latitude)
+    : longitude(longitude), latitude(latitude) {}
+
+Position::Position(double local_x, double local_y, double reference_longitude, double reference_latitude) {
+    set_local_coordinates(local_x, local_y, reference_longitude, reference_latitude);
 }
 
-std::tuple<double, double> Position::get_longitude_latitude() {
-    return std::make_tuple(longitude, latitude);
+void Position::set_longitude_latitude(double longitude, double latitude) {
+    this->longitude = longitude;
+    this->latitude = latitude;
 }
 
-std::tuple<double, double> Position::get_latitude_longitude() {
-    return std::make_tuple(latitude, longitude);
+std::array<double, 2> Position::get_longitude_latitude() const {
+    return {longitude, latitude};
 }
 
-void Position::set_longitude_latitude(double longitude_, double latitude_) {
-    longitude = longitude_;
-    latitude = latitude_;
+std::array<double, 2> Position::get_latitude_longitude() const {
+    return {latitude, longitude};
 }
 
 void Position::set_local_coordinates(double local_x, double local_y, double reference_longitude, double reference_latitude) {
-    std::array<double, 3> lla_vector = ned2lla(std::array<double, 3>({local_y, local_x, 0}), reference_latitude, reference_longitude, 0);
-    latitude = lla_vector[0];
-    longitude = lla_vector[1];
+    // navpy style ned_vector is [North, East, Down]
+    // local_x is North, local_y is East
+    std::array<double, 3> lla_vector = ned2lla({local_x, local_y, 0.0}, reference_latitude, reference_longitude, 0.0);
+    this->latitude = lla_vector[0];
+    this->longitude = lla_vector[1];
 }
 
-std::array<float, 2> Position::get_local_coordinates(double reference_longitude, double reference_latitude) {
-    std::array<float, 3> ned_vector = lla2ned(latitude, longitude, 0, reference_latitude, reference_longitude, 0);
-    return std::array<float, 2>({ned_vector[1], ned_vector[0]});
+std::array<double, 2> Position::get_local_coordinates(const std::array<double, 2>& reference_longitude_latitude) const {
+    double reference_longitude = reference_longitude_latitude[0];
+    double reference_latitude = reference_longitude_latitude[1];
+
+    // lla2ned returns [North, East, Down]
+    std::array<float, 3> ned_vector = lla2ned(this->latitude, this->longitude, 0.0, reference_latitude, reference_longitude, 0.0);
+    
+    // Return [North, East] which corresponds to [local_x, local_y]
+    return {static_cast<double>(ned_vector[0]), static_cast<double>(ned_vector[1])};
 }
--- src/autopilot_cpp/autopilot_library/position.hpp
+++ src/autopilot_cpp/autopilot_library/position.hpp
@@ -5,28 +5,73 @@
 #include <array>
 
 
-/*
-A position that describes a point on the earth which is stored internally as its longitude and latitude.
-Whenever a function such as get_local_coordinates is called, this class has to convert to the proper position measurement system such as NED or UTM.
-
-This class mainly just calls on python libraries such as navpy, utm, and pygeodesy to convert to other position measurement systems.
-*/
+/**
+ * A position that describes a point on the earth which is stored internally as its longitude and latitude.
+ * 
+ * Whenever a function such as get_local_coordinates is called, this class has to convert to the proper
+ * position measurement system such as NED or UTM.
+ * 
+ * This class mainly just calls on internal geographic functions to convert to other position measurement systems.
+ * 
+ * The constructor supports two mutually exclusive initialization schemes:
+ * 1. Global: Provide longitude and latitude only.
+ *    Position(longitude, latitude)
+ * 2. Local: Provide local_x, local_y, and a reference global point (reference_longitude, reference_latitude).
+ *    Position(local_x, local_y, reference_longitude, reference_latitude)
+ */
 class Position {
-    
-    
 public:
+    double longitude; ///< The global longitude in degrees.
+    double latitude;  ///< The global latitude in degrees.
 
-    double longitude, latitude;
+    /**
+     * Global initialization: Sets the position using longitude and latitude.
+     * @param longitude The global longitude in degrees.
+     * @param latitude The global latitude in degrees.
+     */
+    Position(double longitude, double latitude);
 
-    Position(double longitude_, double latitude_);
+    /**
+     * Local initialization: Sets the position using local NED coordinates and a reference point.
+     * @param local_x The Cartesian x-coordinate (NED North) relative to the reference point.
+     * @param local_y The Cartesian y-coordinate (NED East) relative to the reference point.
+     * @param reference_longitude The longitude of the origin for local coordinates.
+     * @param reference_latitude The latitude of the origin for local coordinates.
+     */
+    Position(double local_x, double local_y, double reference_longitude, double reference_latitude);
 
-    std::tuple<double, double> get_longitude_latitude();
-    std::tuple<double, double> get_latitude_longitude();
-    void set_longitude_latitude(double longitude_, double latitude_);
+    /**
+     * Sets the position using longitude and latitude.
+     * @param longitude The longitude to set the position to.
+     * @param latitude The latitude to set the position to.
+     */
+    void set_longitude_latitude(double longitude, double latitude);
 
-    // TODO: DOUBLE CHECK TO MAKE SURE THAT LATITUDE AND LONGITUDE ARE BEING RETURNED AND PROCESSED IN THE CORRECT ORDER
-    // TODO: ALSO CLEAN THIS UP AND MAKE IT LOOK NICER
-    // TODO: ALSO MAYBE CLEAN UP THE NED2LLA FUNCTION INTERFACE AND MAKE IT LOOK MORE LIKE THE LLA2NED FUNCTION INTERFACE
+    /**
+     * Gets the longitude and latitude of this position.
+     * @return Array where the first element is longitude and the second is latitude.
+     */
+    std::array<double, 2> get_longitude_latitude() const;
+
+    /**
+     * Gets the latitude and longitude of this position.
+     * @return Array where the first element is latitude and the second is longitude.
+     */
+    std::array<double, 2> get_latitude_longitude() const;
+
+    /**
+     * Sets the position using local NED coordinates.
+     * @param local_x The local x coordinate (north).
+     * @param local_y The local y coordinate (east).
+     * @param reference_longitude The reference longitude for the NED frame.
+     * @param reference_latitude The reference latitude for the NED frame.
+     */
     void set_local_coordinates(double local_x, double local_y, double reference_longitude, double reference_latitude);
-    std::array<float, 2> get_local_coordinates(double reference_longitude, double reference_latitude);
+
+    /**
+     * Gets the local NED coordinates of this position with respect to a reference position.
+     * @param reference_longitude_latitude Array where [0] is reference longitude and [1] is reference latitude.
+     * @return Array where [0] is local x (North) and [1] is local y (East).
+     */
+    std::array<double, 2> get_local_coordinates(const std::array<double, 2>& reference_longitude_latitude) const;
 };
