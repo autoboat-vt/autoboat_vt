0b9a6192b8c0895f30a2cb62166bf4b81e93588b
Barrett Wise <barrettw@fastmail.com>
2026-04-19 19:08:10 -0400

    fix add_500_test_waypoints button not work when clicked two times in a row

--- ground_station/src/widgets/groundstation.py
+++ ground_station/src/widgets/groundstation.py
@@ -182,6 +182,7 @@ class GroundStationWidget(QWidget):
             lambda: self.edit_telemetry_config_window.show() or self.edit_telemetry_config_window.raise_()
         )
 
+        self.test_waypoint_rng = np.random.default_rng(69420)
         self.add_500_test_waypoints_button = QPushButton("Add 500 Test Waypoints?")
         self.add_500_test_waypoints_button.clicked.connect(self.add_500_test_waypoints)
 
@@ -354,10 +355,9 @@ class GroundStationWidget(QWidget):
     def add_500_test_waypoints(self) -> None:
         """Add 500 test waypoints to the map."""
 
-        generator = np.random.default_rng(seed=69420)
         for _ in range(500):
-            latitude = generator.uniform(-90, 90)
-            longitude = generator.uniform(-180, 180)
+            latitude = self.test_waypoint_rng.uniform(-90, 90)
+            longitude = self.test_waypoint_rng.uniform(-180, 180)
             self.browser.page().runJavaScript(f"map.add_waypoint({latitude}, {longitude})")
 
         print("[Info] Added 500 test waypoints to the map, LOL.")
