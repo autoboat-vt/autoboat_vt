daf865d8ae1930d8566c2153d229186bea025719
kyle-rodriguez13 <kyle1989tv@gmail.com>
2026-05-02 11:38:13 -0400

    Finish up some more functions.

--- src/microros/src/UART.cpp
+++ src/microros/src/UART.cpp
@@ -32,12 +32,54 @@ static uart_inst_t * uart_get_instance (uint num){
 }
 //Note: incline for used for small function:
 static incline uart_hw_t * uart_get_hw (uart_inst_t *uart){
-    //Gets HARDWARE number as an reference number 
+     if (!uart) {
+        return NULL;
+     }
+     //THis is the pointer value 
+    return uart->hw;
     
 
 }
 uint uart_init (uart_inst_t *uart, uint baudrate){
-    //Installs the UART file 
+    if (!uart || baudrate == 0){
+     return 0;
+    }
+    //First lets get the hardware pointer 
+    uart_hw_t *hw = uart_get_hw(uart);
+
+    //Now lets set up the baudrate 
+    uint actual_baud = uart_set_baudrate(uart, baudrate);
+
+    //lets set it up 
+    hw->cr = 1;
+    
+    return actual_baud ; 
 }
+
+void uart_deinit(uart_inst_t *uart) {
+    // We get the pointer so we can shut it down ( BLACKPINK refernce)
+    uart_hw_t *hw = uart_get_hw(uart);
+
+    // this disables the UART 
+    hw->cr = 0;
+}
+
+uint uart_set_baudrate(uart_inst_t *uart, uint baudrate) {
+    //Always get the pointer, it makes it simple 
+    uart_hw_t *hw = uart_get_hw(uart);
+
+    
+    uint clk = UART_CLOCK_NUM(uart);
+    //I just researched this by we need the 16x which 
+    uint div = clk / (16 * baudrate);
+
+    // We deference it to get the baudrate 
+    hw->ibrd = div;
+
+    return baudrate;
+}
+
+
+
 
 
